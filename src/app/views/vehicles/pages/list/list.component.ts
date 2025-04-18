12282b2181f3f611506a4c9d6fe46efe938a7b0b
Eduarda F A Costa <eduarda.fernandes.costa01@gmail.com>
2025-04-18 11:56:17 -0300

    feat: ✨ add basic listing features to the main page

--- src/app/views/vehicles/pages/list/list.component.ts
+++ src/app/views/vehicles/pages/list/list.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { Vehicle } from '../../model/vehicle.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator'
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit, AfterViewInit {
+
+  vehicles: Vehicle[] = [
+    {
+      id: 0,
+      placa: 'ABC1D23',
+      chassi: '1f1 6FPSA8 He 6C1581',
+      renavam: '22193945321',
+      modelo: 'Limousine',
+      marca: 'Ford',
+      ano: 2009,
+    },
+    {
+      id: 0,
+      placa: 'ABC1D23',
+      chassi: '1f1 6FPSA8 He 6C1581',
+      renavam: '22193945321',
+      modelo: 'Limousine',
+      marca: 'Ford',
+      ano: 2009,
+    },
+    {
+      id: 0,
+      placa: 'ABC1D23',
+      chassi: '1f1 6FPSA8 He 6C1581',
+      renavam: '22193945321',
+      modelo: 'Limousine',
+      marca: 'Ford',
+      ano: 2009,
+    },
+    {
+      id: 0,
+      placa: 'ABC1D23',
+      chassi: '1f1 6FPSA8 He 6C1581',
+      renavam: '22193945321',
+      modelo: 'Limousine',
+      marca: 'Ford',
+      ano: 2009,
+    },
+  ];
+  displayedColumns: string[] = ['placa', 'chassi', 'renavam', 'modelo', 'marca', 'ano', 'actions'];
+
+  dataSource: MatTableDataSource<Vehicle> = new MatTableDataSource<Vehicle>();
+  @ViewChild(MatPaginator, { static: false }) paginator : MatPaginator = new MatPaginator(new MatPaginatorIntl(), ChangeDetectorRef.prototype);
+  @ViewChild(MatSort, { static: false }) sort: MatSort = new MatSort();
+
+  ngOnInit(): void {
+    this.dataSource = new MatTableDataSource(this.vehicles); //Atualiza os valores da tabela com o this.vehicles
+    this.setTableConfigs();
+  }
+
+  ngAfterViewInit() {
+    this.setTableConfigs(); // Configura Paginação e Ordenação da tabela
+  }
+
+  setTableConfigs() {
+
+    let intl = new MatPaginatorIntl();
+
+    //Configurando a tradução da paginação da tabela, por padrão vem em inglês
+    intl.itemsPerPageLabel = 'Items por página:';
+    intl.firstPageLabel = 'Primeira página';
+    intl.previousPageLabel = 'Página anterior';
+    intl.nextPageLabel = 'Próxima página';
+    intl.lastPageLabel = 'Última página';
+
+    this.paginator._intl = intl;
+
+    //Atualizando paginação e ordenação da tabela
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 
 }
--- src/app/views/vehicles/vehicles.module.ts
+++ src/app/views/vehicles/vehicles.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VehiclesRoutingModule } from './vehicles-routing.module';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//  Imports dos componentes criados
 import { ListComponent } from './pages/list/list.component';
 import { DetailComponent } from './pages/detail/detail.component';
 import { EditComponent } from './pages/edit/edit.component';
 import { AddComponent } from './pages/add/add.component';
 import { DeleteDialogComponent } from './components/delete-dialog/delete-dialog.component';
+
+// Imports do Angular Material
+import {MatTableModule} from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatIconModule} from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSortModule } from '@angular/material/sort';
 
 
 @NgModule({
@@ -19,7 +30,13 @@ import { DeleteDialogComponent } from './components/delete-dialog/delete-dialog.
   ],
   imports: [
     CommonModule,
-    VehiclesRoutingModule
+    VehiclesRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatIconModule,
+    MatTooltipModule
   ]
 })
 export class VehiclesModule { }
